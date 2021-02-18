8f9deffdd5cb3c26f798f04f04287e774dd67eec
adyshkiss@outlook.com <adyshkiss@outlook.com>
2021-02-18 12:17:28 +0300

    Добавьте файлы проекта.

---
+++ ISP9_5/AddEditWin.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ISP9_5
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditWin.xaml
+    /// </summary>
+    public partial class AddEditWin : Window
+    {
+        Entities context = new Entities();
+
+        public AddEditWin()
+        {
+            InitializeComponent();
+            tbTitle.Text = "Добавление";
+            tbID.Text = string.Empty;
+        }
+
+        public AddEditWin(Person person)
+        {
+            InitializeComponent();
+            tbTitle.Text = "Изменение";
+            btnAddPerson.Content = "Сохранить";
+            tbID.Text += person.ID;
+
+            txtFname.Text = person.FirstName;
+            txtLname.Text = person.LastName;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                context.Person.Add(new Person
+                {
+                    LastName = txtLname.Text,
+                    FirstName = txtFname.Text
+                });
+                context.SaveChanges();
+
+                MessageBox.Show("Запись успешно добавлена");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }           
+           
+        }
+    }
+}
---
+++ ISP9_5/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISP9_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Entities context = new Entities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgPerson.ItemsSource = context.Person.ToList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditWin addEditWin = new AddEditWin();
+            this.Hide();
+            addEditWin.ShowDialog();
+            dgPerson.ItemsSource = context.Person.ToList();
+            this.Show();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var resultMess = MessageBox.Show("ВЫ УВЕРЕНЫ?", "Удаление пользователя", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (resultMess == MessageBoxResult.Yes)
+            {
+                if (dgPerson.SelectedItem is Person person)
+                {
+                    context.Person.Remove(person);
+                    context.SaveChanges();
+                    MessageBox.Show("Запись удалена");
+                    dgPerson.ItemsSource = context.Person.ToList();
+                }
+                else
+                {
+                    MessageBox.Show("Запись не выбрана");
+                }
+            }
+            
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgPerson.SelectedItem is Person person)
+            {
+                AddEditWin addEditWin = new AddEditWin(person);
+                this.Hide();
+                addEditWin.ShowDialog();
+                dgPerson.ItemsSource = context.Person.ToList();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Запись не выбрана");
+            }
+
+
+        }
+    }
+}
